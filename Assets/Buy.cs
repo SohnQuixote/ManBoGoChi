2f8cdfead75925992795512a31646e568a60c0b0
DESKTOP-AOME72U\JDK <1217jdk@naver.com>
2021-05-28 16:37:02 +0900

    itemroot 관련 주석 추가

--- Assets/Buy.cs
+++ Assets/Buy.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Buy : MonoBehaviour
 {
     public Transform SelectedItem;
-    public Transform ItemRoot;
+    public Transform ItemRoot;          //유니티 인벤토리씬에 있는 ItemList 오브젝트를 가리킴
 
     private int temp;
     public void OnClickBuy()
     {
         var selecteditem = SelectedItem.GetComponent<ItemIndex>();
-        var item = ItemRoot.GetChild(selecteditem.index).GetComponent<Item>();
+        var item = ItemRoot.GetChild(selecteditem.index).GetComponent<Item>();       //ItemList 오브젝트에서 선택된 아이템을 반환
         temp = PlayerPrefs.GetInt(item.name, 0);
         temp += 1;
         PlayerPrefs.SetInt(item.name, temp);
--- Assets/Store.cs
+++ Assets/Store.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class Store : MonoBehaviour
 {
     public ItemBuffer itemBuffer;
-    public Transform ItemRoot;
+    public Transform ItemRoot;          //유니티 상점씬에 있는 ItemList 오브젝트를 가리킴
 
-    private List<Item> itemList;
+    private List<Item> itemList;        //스크립트에서 사용할 item을 원소로 가지는 List
 
     // Start is called before the first frame update
     void Start()
     {
         itemList = new List<Item>();
 
-        int itemCount = ItemRoot.childCount;
+        int itemCount = ItemRoot.childCount;        //ItemList 오브젝트 내부에 몇개의 하위 오브젝트가 있는지 반환. 즉, 상점의 ItemList 오브젝트 내부에 있는 item 오브젝트의 수를 나타냄
 
         for (int i = 0; i < itemCount; i++)
         {
-            var item = ItemRoot.GetChild(i).GetComponent<Item>();
+            var item = ItemRoot.GetChild(i).GetComponent<Item>();       //ItemList 오브젝트에서 i번째 item 오브젝트를 반환
             
             if (i < itemBuffer.items.Count)
             {
