300768c958dbc8cd6125fede6cd285f7f6fd931b
SohnQuixote <80592435+SohnQuixote@users.noreply.github.com>
2021-05-30 01:24:49 +0900

    bombGraphicAdded

---
+++ Assets/Scenes/Scripts/GoToMiniGameScene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GoToMiniGameScene : MonoBehaviour
+{
+    public void GoMini()
+    {
+        SceneManager.LoadScene("MiniGameScene");
+    }
+}
