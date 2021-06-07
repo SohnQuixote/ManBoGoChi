08bfbb053fc5a0bcea1ff8361fc4f5e2a457dbd1
DESKTOP-AOME72U\JDK <1217jdk@naver.com>
2021-06-08 01:57:02 +0900

    WP 저장, WP를 이용한 아이템 구매, 아이템 효과와 가격 설정, 아이템 사용 구현

--- Assets/Store.cs
+++ Assets/Store.cs
@@ -6,12 +6,15 @@ public class Store : MonoBehaviour
 {
     public ItemBuffer itemBuffer;
     public Transform ItemRoot;          //유니티 상점씬에 있는 ItemList 오브젝트를 가리킴
+    public UnityEngine.UI.Text mymoney;
 
     private List<Item> itemList;        //스크립트에서 사용할 item을 원소로 가지는 List
 
     // Start is called before the first frame update
     void Start()
     {
+        mymoney.text = PlayerPrefs.GetInt("WP", 0).ToString() + "WP";
+
         itemList = new List<Item>();
 
         int itemCount = ItemRoot.childCount;        //ItemList 오브젝트 내부에 몇개의 하위 오브젝트가 있는지 반환. 즉, 상점의 ItemList 오브젝트 내부에 있는 item 오브젝트의 수를 나타냄
